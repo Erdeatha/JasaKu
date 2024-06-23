1a2acf083765f5a2a367b85abe87fffc9191e142
Danis Keysara Saputra <daniskeysara@gmail.com>
2024-06-24 00:04:37 +0700

    benerin tampilan home

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -14,6 +14,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
     <script src="path/to/jquery.js"></script>
 
     <!-- Ajax -->
@@ -25,11 +28,7 @@
 </head>
 <body>
     <?= $this->include('layout/navbar'); ?>
-
-    <main>
-        <?= $this->renderSection('content'); ?>
-    </main>
-
+    <?= $this->renderSection('content'); ?>
     <?= $this->include('layout/footer'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
